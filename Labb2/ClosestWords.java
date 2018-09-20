de8d645a6d59c6f93af2877ddec6d6b94c85baf2
fkam <felix.kam@live.se>
2018-09-20 09:09:15 +0200

    minor change in ClosestWords

--- Labb2/ClosestWords.java
+++ Labb2/ClosestWords.java
@@ -3,23 +3,19 @@
 /* Ursprunglig författare: Viggo Kann KTH viggo@nada.kth.se      */
 import java.util.LinkedList;
 import java.util.List;
+import java.lang.Math;
 
 public class ClosestWords {
-  static LinkedList<String> closestWords = new LinkedList<String>();
-
+  static LinkedList<String> closestWords = null;
   int closestDistance = -1;
+  int[][] dist = new int[41][41];
+  String prevWord = null;
 
-  int[][] partDist1(String w1, String w2, int w1len, int w2len){
-    int[][] dist = new int[w1len+1][w2len+1];
-    int i, j, res, add, del;
-
-    for (i = 0; i <= w1len; i++)
-        dist[i][0] = i;
-    for (j = 0; j <= w2len; j++)
-        dist[0][j] = j;    
-
-    for (i = 1; i <= w1len; i++){
-        for (j = 1; j <= w2len; j++){
+  int partDist1(String w1, String w2, int w1len, int w2len){
+    int i, j, res, add, del, start;
+    start = checkPrev(w1);
+    for (i = start; i <= w1len; i++){
+        for (j = start; j <= w2len; j++){
           res = dist[i-1][j-1] + charCompare(w1, w2, i-1, j-1);
           add = dist[i-1][j] + 1;
           del = dist[i][j-1] + 1;
@@ -27,15 +23,32 @@ public class ClosestWords {
         }
     }
 
-    return dist;
+    return dist[w1len][w2len];
+  }
+
+  int checkPrev(String word){
+    int counter = 0, len;
+    if (prevWord == null){
+        prevWord = word;
+        return 1;
+    }
+    len = prevWord.length();
+    if (prevWord.length() > word.length())
+        len = word.length();
+    for (int i = 0; i < len; i++){
+        if (charCompare(prevWord, word, i, i) == 0)
+            counter++;
+        else if (counter == 0){
+            prevWord = word;
+            return 1; 
+        } else 
+            return counter;
+    }
+    return counter; 
   }
 
   int min(int res, int add, int del){
-    if (add < res)
-        return add;
-    else if(del < res)
-        return del;
-    return res;
+    return Math.min(Math.min(res, add), del);
   }
 
   int charCompare(String w1, String w2, int pos1, int pos2){
@@ -59,31 +72,19 @@ public class ClosestWords {
   }
 
   int Distance(String w1, String w2) {
-//    return partDist(w1, w2, w1.length(), w2.length());
-    int v = partDist(w1, w2, w1.length(), w2.length());
-    System.out.println("OLD: ");
-    for (int i = 0; i <= w1.length(); i++){
-        for (int j = 0; j <= w2.length(); j++){
-            System.out.println(partDist(w1, w2, i, j)); //+ " " + w1.substring(0,i) + " " + w2.substring(0,j));
-        }
-
-        System.out.println();
-    }
-    System.out.println("NEW: ");
-    int test[][] = partDist1(w1, w2, w1.length(), w2.length());
-    for (int i = 0; i <=w1.length(); i++){
-        for (int j = 0; j <= w2.length(); j++){
-            System.out.println(test[i][j]);
-        }
-        System.out.println();
+    return partDist1(w1, w2, w1.length(), w2.length());
     }
 
 
   
-    return v;
-  }
 
   public ClosestWords(String w, List<String> wordList) {
+
+    for (int i = 0; i <= 40; i++){
+        dist[i][0] = i;
+        dist[0][i] = i;
+    }
+
     for (String s : wordList) {
       int dist = Distance(w, s);
       // System.out.println("d(" + w + "," + s + ")=" + dist);
@@ -105,9 +106,5 @@ public class ClosestWords {
     return closestWords;
   }
 
-  public static void main(String[] args){
-    closestWords.add(0, args[0].toLowerCase());
-    new ClosestWords(args[1].toLowerCase(), closestWords);
-  }
 
 }
